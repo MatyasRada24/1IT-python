a1cc68212600e14df65a0a4eaf8278b0ec741906
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-03-11 16:35:48 +0100

    Add files via upload

---
+++ vypocetDelkyZivota.py
@@ -0,0 +1,6 @@
+age = int(input("Zadej, kolik je ti let. "))
+remain = 80 - age
+months = 12 * remain
+weeks = 52 * remain
+days = 365 * remain
+print(f"Zbývá ti {days} dní, {weeks} týdnů nebo {months} měsíců a {remain} let.")
