88ed0a5f65c7ef4894e5ff28bc853ffa8b7fb632
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-03-11 16:36:39 +0100

    vypocetDelkyZivota.py

--- vypocetDelkyZivota.py
+++ vypocetDelkyZivota.py
@@ -3,4 +3,4 @@ remain = 80 - age
 months = 12 * remain
 weeks = 52 * remain
 days = 365 * remain
-print(f"Zbývá ti {days} dní, {weeks} týdnů nebo {months} měsíců a {remain} let.")
+print(f"Zbývá ti {days} dní, {weeks} týdnů nebo {months} měsíců a {remain} let do 80.")
