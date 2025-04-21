c80cebfe026fdf01c18b336bce65cacfdf99d479
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-04-21 14:48:51 +0200

    Add files via upload

---
+++ dwadada.py
@@ -0,0 +1,6 @@
+n = int(input("Napš číslo: "))
+vypocet = n / 5
+vysledek = print(int(vypocet))
+if n % 5 == 0: print("Číslo je dělitelné 5")
+if n % 10 == 0: print("Číslo je dělitelné 10")
+else: print("Číslo není dělitelné 10.")
