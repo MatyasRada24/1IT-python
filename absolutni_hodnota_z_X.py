ae2c10a17cf268e74aaecadf2ca6fba559b09974
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-04-23 16:58:06 +0200

    Update absolutni_hodnota_z_X.py

--- absolutni_hodnota_z_X.py
+++ absolutni_hodnota_z_X.py
@@ -1,2 +1,11 @@
+#Jednodušží cesta.
 n = 100
 print(abs(n))
+
+#Složitější cesta.
+n = int(input("Zadej číslo: "))
+AH = 0
+if AH < n:
+    print("Absolutní hodnota je " + str(n))
+if AH > n:
+    print("Absolutní hodnota je " + str(-n))
