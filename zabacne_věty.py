d5142b5c40edd27d5b5706a090c3ebd36a816af7
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-04-29 16:01:08 +0200

    Add files via upload

---
+++ zabacne_věty.py
@@ -0,0 +1,7 @@
+zvíře = input("Zadej osobu/zvíře: \n ")
+co_dělal = input("Co dělala ta osoba/zvíře? ")
+jak_jsem_reagoval = input("Co jsi teda dělal? \n ")
+
+print(f"Dneska jsem šel do zoo a viděl jsem tam{zvíře}.")
+print(f"Ten {co_dělal}.")
+print(f"Potom co {co_dělal} tak jsem ho/mu {jak_jsem_reagoval}.")
