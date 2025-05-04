5c12f907a3de0e51cb33d3d19551367cbc44e9a1
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-05-04 17:12:01 +0200

    Add files via upload

---
+++ kalkulacka.py
@@ -0,0 +1,15 @@
+x = int(input("Zadejte první číslo: "))
+y = int(input("Zadejte druhé číslo: "))
+operace = input("Jakou operaci chceš provést? ")
+if operace == "+":
+    print(x + y)
+elif operace == "-":
+    print(x - y)
+elif operace == "*":
+    print(x * y)
+elif operace == "/":
+    print(x / y)
+elif operace == "**":
+    print(x ** y)
+elif operace == "//":
+    print(x // y)
