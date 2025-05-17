39712cb74a77924c3f13d2a4f2894a9baca546a4
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-05-17 20:03:17 +0200

    Update BMI_calculator_V2.0.py

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,4 +1,10 @@
-mass = input("Napiš svojí hmotnost v kg. ")
-height = input("Napiš svou výšku v metrech. ")
-BMI = int(mass) / float(height)**2
-print(BMI)
+mass = input("Napiš svojí hmotnost v kg: ")
+height = input("Napiš svou výšku v metrech: ")
+BMI = float(mass) / float(height)**2
+
+if BMI < 18.5:
+    print(f"Tvoje BMI je {BMI:.2f}, máš podváhu.")
+elif 18.5 <= BMI <= 25:
+    print(f"Tvoje BMI je {BMI:.2f}, máš normální váhu.")
+else:  # tedy BMI > 25
+    print(f"Tvoje BMI je {BMI:.2f}, máš nadváhu.")
