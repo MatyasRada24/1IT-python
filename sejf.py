757ddde8aca26e5dd73de6c1f4dd4c673d1a61ec
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-05-20 16:16:01 +0200

    Update sejf.py

--- sejf.py
+++ sejf.py
@@ -3,8 +3,7 @@ for i in range(2, 10, 2):
     for j in range(1, 10):
         for k in range(1, 10):
             for l in range(1, 5):
-             if i + j + k + l == 21:
-                print(i, j, k, l)
-                count += 1
-print(f"Počet cifer:{count}.")
-# cifry se nesmí opakovat.
+                if i + j + k + l == 21 and i != j and i != k and i!= l and j != k and j != l and k != l:
+                 print(i, j, k, l)
+                 count += 1
+print(f"Počet cifer: {count}.")
