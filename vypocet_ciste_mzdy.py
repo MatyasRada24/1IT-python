37f983946ef5e1497d8bd1db566d06fa38217398
MatyasRada24 <matyas.rada024@skolavdf.cz>
2025-07-09 14:07:32 +0200

    Update vypocet_ciste_mzdy.py

--- vypocet_ciste_mzdy.py
+++ vypocet_ciste_mzdy.py
@@ -7,7 +7,7 @@ print("Za 3 děti je sleva 2320Kč")
 print("Stát dává pouze slevu na max 3 děti. Pokud máš více než 3 děti, sleva na dani se nezvyšuje.")
 print("------------------------------------------------------------------------------------------------")
 
-vyplata = int(input("Zadej svojí hrubou mzdu: "))
+vyplata = float(input("Zadej svojí hrubou mzdu: "))
 dan = int(input("Vyber daň: (15 / 23) "))
 deti = int(input("Kolik máš dětí? (0-3) "))
 
@@ -16,8 +16,12 @@ zdravko = vyplata * 0.045 # - ZDRAVKO
 superhruba = vyplata * 1.338
 sleva_poplatnik = 2570
 
+if vyplata == " ":
+    vyplata.replace(" ", "")
+
 if dan == 15 and deti == 0:
-    vypocet = vyplata - (socko + zdravko) - (superhruba * 0.15 - sleva_poplatnik)
+    vypocet = vyplata - socko - zdravko - max(0, superhruba * 0.15 - sleva_poplatnik)
+
     print(f"Čistá mzda je: {vypocet} Kč")
 else:("Něco se pokazilo, zkus to znovu.")
 
@@ -35,6 +39,22 @@ if dan == 15 and deti == 3:
     vypocet3 = vyplata - (socko + zdravko) - (superhruba * 0.15 - sleva_poplatnik - 1267 - 1860 - 2320)
     print(f"Čistá mzda je: {vypocet3} Kč")
 else:("Něco se pokazilo, zkus to znovu.")
+
+if dan == 23 and deti == 0:
+    vypocet4 = vyplata - (socko + zdravko) - (superhruba * 0.23 - sleva_poplatnik)
+    print(f"Čistá mzda je: {vypocet4} Kč")
+
+if dan == 23 and deti == 1:
+    vypocet5 = vyplata - (socko + zdravko) - (superhruba * 0.23 - sleva_poplatnik - 1267)
+    print(f"Čistá mzda je: {vypocet5} Kč")
+
+if dan == 23 and deti == 2:
+    vypocet6 = vyplata - (socko + zdravko) - (superhruba * 0.23 - sleva_poplatnik - 1267 - 1860)
+    print(f"Čistá mzda je: {vypocet6} Kč")
+
+if dan == 23 and deti == 3:
+    vypocet7 = vyplata - (socko + zdravko) - (superhruba * 0.23 - sleva_poplatnik - 1267 - 1860 - 2320)
+    print(f"Čistá mzda je: {vypocet7} Kč")
 
 if vyplata >= 131901 and dan == 15:
     print("Máš větší příjem než 131 901Kč, tuďíš se na tebe vztahuje 23% daň.")
